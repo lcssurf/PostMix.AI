f612b94b1251c2248015bed62a66fd7ea27033e0
lcssurf <lucasidalinor@gmail.com>
2025-05-14 15:42:06 -0300

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,23 +1,23 @@
-import { blogs, docs } from "@/app/source";
+// import { blogs, docs } from "@/app/source";
 import { publicRoutes, siteUrls } from "@/config/urls";
 import type { MetadataRoute } from "next";
 
 const addPathToBaseURL = (path: string) => `${siteUrls.publicUrl}${path}`;
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-    const blogsSitemap = blogs.getPages().map((blog) => ({
-        url: addPathToBaseURL(blog.url),
-        lastModified: blog.data.exports.lastModified
-            ? new Date(blog.data.exports.lastModified)
-            : new Date(blog.data.publishedAt),
-    }));
+    // const blogsSitemap = blogs.getPages().map((blog) => ({
+    //     url: addPathToBaseURL(blog.url),
+    //     lastModified: blog.data.exports.lastModified
+    //         ? new Date(blog.data.exports.lastModified)
+    //         : new Date(blog.data.publishedAt),
+    // }));
 
-    const docsSitemap = docs.getPages().map((doc) => ({
-        url: addPathToBaseURL(doc.url),
-        lastModified: doc.data.exports.lastModified
-            ? new Date(doc.data.exports.lastModified)
-            : undefined,
-    }));
+    // const docsSitemap = docs.getPages().map((doc) => ({
+    //     url: addPathToBaseURL(doc.url),
+    //     lastModified: doc.data.exports.lastModified
+    //         ? new Date(doc.data.exports.lastModified)
+    //         : undefined,
+    // }));
 
     const publicRoutesWithoutPublicUrl = publicRoutes.filter(
         (route) =>
@@ -29,5 +29,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
         lastModified: new Date(),
     }));
 
-    return [...routes, ...blogsSitemap, ...docsSitemap];
+    return [...routes
+        // , ...blogsSitemap, ...docsSitemap
+    ];
 }
